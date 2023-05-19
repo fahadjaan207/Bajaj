ea0c3b6ab69cf3633b7f694fcca798675e1fa884
S M Fahad Jaan <sj6170@srmist.edu.in>
2023-05-19 18:41:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [developers, setDevelopers] = useState([]);
+  const [filteredDevelopers, setFilteredDevelopers] = useState([]);
+  const [searchName, setSearchName] = useState('');
+  const [searchDesignation, setSearchDesignation] = useState('');
+
+  useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json')
+      .then(response => {
+        setDevelopers(response.data);
+        setFilteredDevelopers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    filterDevelopers();
+  }, [searchName, searchDesignation]);
+
+  const filterDevelopers = () => {
+    let filtered = developers;
+
+    if (searchName) {
+      filtered = filtered.filter(developer =>
+        developer.name.toLowerCase().includes(searchName.toLowerCase())
+      );
+    }
+
+    if (searchDesignation) {
+      filtered = filtered.filter(developer =>
+        developer.designation.toLowerCase().includes(searchDesignation.toLowerCase()) ||
+        developer.skills.join(' ').toLowerCase().includes(searchDesignation.toLowerCase())
+      );
+    }
+
+    setFilteredDevelopers(filtered);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Developer List</h1>
+      <div>
+        <label htmlFor="name">Search by Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+          placeholder="Enter name..."
+        />
+        <br /><br />
+        <label htmlFor="designation">Filter by Designation/Skills:</label>
+        <input
+          type="text"
+          id="designation"
+          value={searchDesignation}
+          onChange={(e) => setSearchDesignation(e.target.value)}
+          placeholder="Enter designation/skills..."
+        />
+      </div>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Skills</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredDevelopers.map((developer, index) => (
+            <tr key={index}>
+              <td>{developer.name}</td>
+              <td>{developer.designation}</td>
+              <td>{developer.skills.join(', ')}</td>
+              <td>{developer.email}</td>
+              <td>{developer.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
